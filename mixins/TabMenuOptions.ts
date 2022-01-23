3e76b8672450e8c7779e8cf0b839afc3647fe588
peachbranch777 <peachbranch.0517@gmail.com>
2022-01-23 18:27:00 +0900

    feat: 🎸  add TabMenuOption

---
+++ mixins/TabMenuOptions.ts
@@ -0,0 +1,20 @@
+import { TabMenuOption } from '~/types/front-type';
+import useUrls from '~/utils/useUrls';
+
+export const tabMenuOptions: TabMenuOption[] = [
+  {
+    label: 'Home',
+    link: useUrls.topLink,
+    isActive: 'Home',
+  },
+  {
+    label: 'Post',
+    link: useUrls.topLink,
+    isActive: 'Post',
+  },
+  {
+    label: 'Report',
+    link: useUrls.topLink,
+    isActive: 'Report',
+  },
+];
