449b48a261df61352a91c7718ceaaf460be9bd84
peachbranch777 <peachbranch.0517@gmail.com>
2022-01-30 18:28:34 +0900

    feat: 🎸  create app-modal

---
+++ components/molecules/AppModal.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="app-modal">
+    <slot name="modal-content"></slot>
+  </section>
+</template>
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api';
+
+export default defineComponent({
+  setup() {},
+});
+</script>
